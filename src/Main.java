b0d3fb569a5125b33c9db2630425482bf133d9be
Akif <akifabdullayev200512@gmail.com>
2025-01-28 20:36:52 +0400

    change

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
 
         System.out.println("Bye Akif");
+        System.out.println("Bye Akif");
+        System.out.println("Bye Akif");
+        System.out.println("Bye Akif");
 
 
     }
