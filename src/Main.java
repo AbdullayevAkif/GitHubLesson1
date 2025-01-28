421d330fe5d6e9932111194a2cf1aca6f79ccd0c
Akif <akifabdullayev200512@gmail.com>
2025-01-28 20:39:14 +0400

    change

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi Akif");
+        System.out.println("Hi Akif");
+        System.out.println("Hi Akif");
+        System.out.println("Hi Akif");
+        System.out.println("Hi Akif");
 
 
         System.out.println("Bye Akif");
